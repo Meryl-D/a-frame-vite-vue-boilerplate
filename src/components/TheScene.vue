fd9e497e958c05c27bfb26e5b23f4855e939fc08
Meryl Dubois <meryl.dubois@heig-vd.ch>
2024-05-02 13:57:36 +0200

    add personal cesium ion asset test

--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -20,31 +20,53 @@ const allAssetsLoaded = ref(false);
     <TheCameraRig />
 
     <!-- Olivier Ertz's tiles: not working, throws error "tile selected but not loaded" -->
-    <!-- Similar issue opened: https://github.com/nytimes/three-loader-3dtiles/issues/91 -->
-    <a-entity
+    <!-- Maybe the composite (cmpt) format is not supported ? -->
+    <!-- <a-entity
         id="oe-tiles"
         loader-3dtiles="
          url: https://assets.ion.cesium.com/2534736/tileset.json; 
          cesiumIONToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwNGY0Mjk4Ny1kYWMxLTQwZjMtOWM5YS0zZDY0Y2UyYTI5MTciLCJpZCI6MTk3MjAyLCJpYXQiOjE3MDg2MDY5MzN9.0zahHvP9QC7E_C0zRuIDDe_QTPEuUafXfgvRREVXAis;
-         cameraEl: #camera;
-        "
-      ></a-entity>
-
-    <!-- Montreal points cloud: only shows points cloud but no tile is visible -->
-    <!-- Might take a few seconds to load -->
-    <!-- <a-entity
-        id="montreal-tiles"
-        rotation="-90 180 0"
-        position="-300 -200 500"
-        loader-3dtiles="
-         url: https://assets.ion.cesium.com/28945/tileset.json; 
-         cesiumIONToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4YTY0NWRmNy1lY2U4LTRhNWMtYjRiYy1jZTdkOGE3ZDQzYTYiLCJpZCI6MjAwMzM2LCJpYXQiOjE3MDk4MjU1MDR9.qs9k0C_BjJBxCbuYshURSlmGRqsP9JfhVtzn3dOwGE0;
-         cameraEl: #camera;
         "
       ></a-entity> -->
 
+    <!-- Cesium sandcastle asset example with CesiumJS default token (https://github.com/CesiumGS/cesium/blob/main/packages/engine/Source/Core/Ion.js#L6-L7) -->
+    <!-- Works wit small assets (300-400 tiles)  -->
+    <!-- <a-entity
+        id="cesium-token-tiles"
+        rotation="-90 0 0"
+        position="2 10 9"
+        loader-3dtiles="
+         url: https://assets.ion.cesium.com/40866/tileset.json;
+         showStats: true;
+         cesiumIONToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYzAzYzE4NS0yZmJiLTQ5NzUtOWJlYS0zNjdkMzc2ODBmZDgiLCJpZCI6MjU5LCJpYXQiOjE3MTQ1NzE1MDN9.tIhlyfC7MHSWrQmoAqkvQdXMbg3igaC2HIbQp1HKCVM;
+        "
+      ></a-entity> -->
 
-      <!-- NY Times example: working -->
+    <!-- Same asset as above but stored in personal Cesium Ion account (Meryl) + use of personal default token -->
+    <!-- Warning: "Cannot apply a model matrix to bounding volumes of type region. Tileset stays in original geo-coordinates."-->
+    <a-entity
+        id="personal-token-tiles"
+        rotation="-90 0 0"
+        loader-3dtiles="
+         url: https://assets.ion.cesium.com/2562057/tileset.json;
+         showStats: true;
+         cesiumIONToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNzM1YjhiMC0wZGI1LTRjZDctYjMxYi1mMTUyNDQwMDc3YzciLCJpZCI6MjEyNTgxLCJpYXQiOjE3MTQ2NDM3MjV9.TDBv19NG5ggTkGXKLWmqVcCuwodZLreeXiJdmFAqLCQ;
+        "
+      ></a-entity>
+
+      <!-- 3D street lib example: working -->
+      <!-- <a-entity
+          id="cesium-ion-scan"
+          position="-16.246 12.7 1.748"
+          rotation="-90 0 -0.82"
+          loader-3dtiles="url: https://assets.ion.cesium.com/1149944/tileset.json?v=3;
+          showStats: true;
+        cesiumIONToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3YTYyYTIzNS0yODg0LTRhZWQtYjYxNS1iNWNhZGZhNmY3OTgiLCJpZCI6MTcwNjEsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzE1OTcxNzZ9.R8mKo-NizcmOjCN_4dQyd5xO4X655CZZMXa9B5sECyU;"
+          scale="1.33 1.33 1.33"
+        ></a-entity> -->
+
+
+      <!-- NY Times example, doesn't use Cesium: working -->
       <!-- <a-entity
         id="freeman-tiles"
         position="-1 10 0"
@@ -53,7 +75,6 @@ const allAssetsLoaded = ref(false);
         loader-3dtiles="
          url: https://int.nyt.com/data/3dscenes/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json; 
          maximumSSE: 48;
-         cameraEl: #camera;
         "
       >
       </a-entity> -->
